4228192545203a7deec26873871209a96bc8e74c
Nicholas Lanotte <35313792+Nick-L-1996@users.noreply.github.com>
2018-10-24 22:17:56 -0400

    Need to complete part 9

--- ece3849_lab0_starter/buttons.c
+++ ece3849_lab0_starter/buttons.c
@@ -42,11 +42,14 @@ void ButtonInit(void)
     // initialize interrupt controller to respond to timer interrupts
     IntPrioritySet(INT_TIMER0A, BUTTON_INT_PRIORITY);
     IntEnable(INT_TIMER0A);
-    /*
+
     // GPIO PJ0 and PJ1 = EK-TM4C1294XL buttons 1 and 2
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOJ);
-    GPIOPinTypeGPIOInput(GPIO_PORTJ_BASE, GPIO_PIN_0 | GPIO_PIN_1);
-    GPIOPadConfigSet(GPIO_PORTJ_BASE, GPIO_PIN_0 | GPIO_PIN_1, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);
+    GPIOPinTypeGPIOInput(GPIO_PORTJ_BASE, GPIO_PIN_0 | GPIO_PIN_1 );
+    GPIOPadConfigSet(GPIO_PORTJ_BASE, GPIO_PIN_0 | GPIO_PIN_1  , GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);
+
+    //S1, S2 and Select
+
 
     // analog input AIN13, at GPIO PD2 = BoosterPack Joystick HOR(X)
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOD);
@@ -68,7 +71,7 @@ void ButtonInit(void)
     ADCSequenceStepConfigure(ADC0_BASE, 0, 0, ADC_CTL_CH13);                             // Joystick HOR(X)
     ADCSequenceStepConfigure(ADC0_BASE, 0, 1, ADC_CTL_CH17 | ADC_CTL_IE | ADC_CTL_END);  // Joystick VER(Y)
     ADCSequenceEnable(ADC0_BASE, 0);
-    */
+
 }
 
 // update the debounced button state gButtons
@@ -142,26 +145,29 @@ uint32_t ButtonAutoRepeat(void)
 // ISR for scanning and debouncing buttons
 void ButtonISR(void) {
     TimerIntClear(TIMER0_BASE, TIMER_TIMA_TIMEOUT); // clear interrupt flag
-    /*
+
     // read hardware button state
     uint32_t gpio_buttons =
-            ~GPIOPinRead(GPIO_PORTJ_BASE, 0xff) & (GPIO_PIN_1 | GPIO_PIN_0); // EK-TM4C1294XL buttons in positions 0 and 1
+            ~GPIOPinRead(GPIO_PORTJ_BASE, 0xff) & (GPIO_PIN_1 | GPIO_PIN_0  ); // EK-TM4C1294XL buttons in positions 0 and 1
 
     uint32_t old_buttons = gButtons;    // save previous button state
     ButtonDebounce(gpio_buttons);       // Run the button debouncer. The result is in gButtons.
     ButtonReadJoystick();               // Convert joystick state to button presses. The result is in gButtons.
     uint32_t presses = ~old_buttons & gButtons;   // detect button presses (transitions from not pressed to pressed)
     presses |= ButtonAutoRepeat();      // autorepeat presses if a button is held long enough
-    */
+
     static bool tic = false;
     static bool running = true;
-    /*
+
     if (presses & 1) { // EK-TM4C1294XL button 1 pressed
         running = !running;
     }
-    */
+
     if (running) {
         if (tic) gTime++; // increment time every other ISR call
         tic = !tic;
+    }
+    if (presses & 2){
+        gTime = 0;
     }
 }
--- ece3849_lab0_starter/main.c
+++ ece3849_lab0_starter/main.c
@@ -15,9 +15,20 @@
 #include "driverlib/interrupt.h"
 #include "Crystalfontz128x128_ST7735.h"
 #include <stdio.h>
+#include "buttons.h"
 
 uint32_t gSystemClock; // [Hz] system clock frequency
 volatile uint32_t gTime = 8345; // time in hundredths of a second
+
+extern volatile uint32_t gButtons;
+
+int binary_conversion(int num){
+    if(num==0){
+        return 0;
+    }
+    else
+        return(num%2+10*binary_conversion(num/2));
+}
 
 int main(void)
 {
@@ -33,12 +44,16 @@ int main(void)
     Crystalfontz128x128_Init(); // Initialize the LCD display driver
     Crystalfontz128x128_SetOrientation(LCD_ORIENTATION_UP); // set screen orientation
 
+    ButtonInit();
+    IntMasterEnable();
+
     tContext sContext;
     GrContextInit(&sContext, &g_sCrystalfontz128x128); // Initialize the grlib graphics context
     GrContextFontSet(&sContext, &g_sFontFixed6x8); // select font
 
     uint32_t time;  // local copy of gTime
     char str[50];   // string buffer
+    char ButtonIO[50];
     // full-screen rectangle
     tRectangle rectFullScreen = {0, 0, GrContextDpyWidthGet(&sContext)-1, GrContextDpyHeightGet(&sContext)-1};
 
@@ -46,9 +61,17 @@ int main(void)
         GrContextForegroundSet(&sContext, ClrBlack);
         GrRectFill(&sContext, &rectFullScreen); // fill screen with black
         time = gTime; // read shared global only once
-        snprintf(str, sizeof(str), "Time = %06u", time); // convert time to string
+        int ffsec = time%100;
+        int sec = ((time - ffsec)%6000)/100;
+        int min = (time-sec*100-ffsec)/6000;
+
+
+        snprintf(str, sizeof(str), "Time = %02u:%02u:%02u", min, sec, ffsec); // convert time to string
         GrContextForegroundSet(&sContext, ClrYellow); // yellow text
         GrStringDraw(&sContext, str, /*length*/ -1, /*x*/ 0, /*y*/ 0, /*opaque*/ false);
+        int bin = binary_conversion(gButtons);
+        snprintf(ButtonIO, sizeof(ButtonIO), "%09u", bin); // convert time to string
+        GrStringDraw(&sContext, ButtonIO, -1, 0, 20, false);
         GrFlush(&sContext); // flush the frame buffer to the LCD
     }
 }
